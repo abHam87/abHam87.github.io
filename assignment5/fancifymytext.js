8e8f37ea0871232be2f68e957a35c4adf2bd5968
abdullahHamdan7 <hamdanabdullah367@gmail.com>
2023-04-28 01:08:01 -0700

    Update fancifymytext.js

--- assignment5/fancifymytext.js
+++ assignment5/fancifymytext.js
@@ -1,48 +1,42 @@
 
 
-function makeBigger(){
-
-    
+function makeBigger() {
     document.getElementById("inputText-1").style.fontSize = "24pt";
 }
 
-function makeFancy(){
+function makeFancy() {
     //alert("Hello, world")
     const text = document.getElementById("inputText-1");
     text.style.fontWeight = "bold";
     text.style.color = "#0000FF";
     text.style.textDecoration = "underline";
-
-    
 }
 
-function takeFancyOut(){
+function takeFancyOut() {
     //alert("Styles will go away");
     const text = document.getElementById("inputText-1");
     text.style.fontWeight = "24";
     text.style.color = "#000000";
     text.style.textDecoration = "none";
-
 }
-function makeNormal(){
+function makeNormal() {
 
     const text = document.getElementById("inputText-1");
     text.style.fontSize = "10pt";
-    
 }
-function MooChanges(){
-    document.getElementById("inputText-1").value = document.getElementById("inputText-1").value.toUpperCase();
+function MooChanges() {
 
+    document.getElementById("inputText-1").value = document.getElementById("inputText-1").value.toUpperCase();
     const text = document.getElementById("inputText-1").value;
     const words = text.split(" ");
 
     const lastWord = words[words.length - 1];
     const suffix = "-Moo";
-    words[words.length-1] = lastWord + suffix;
+    words[words.length - 1] = lastWord + suffix;
 
-    for(var i=0; i<words.length; i++){
+    for (var i = 0; i < words.length; i++) {
         let word = words[i].trim();
-        word+= "💙";
+        word += "💙";
         words[i] = word;
         //words[words.length-1] = 
     }
